daf67df932c81f6f446740b6039698530ca2d803
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2025-06-26 16:25:16 +0200

    added placeholders and route home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../components/navbar/navbar';
+import { Home } from '../components/home/home';
+import { Footer } from "../components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
